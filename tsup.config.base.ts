7fbd5e6bea7b92c84feda069025e75c4a1ef3e9f
avanish.pathak <avanish.pathak@docusign.com>
2025-02-06 14:25:22 -0800

    feat(build): Add webpack handler for the Shell

---
+++ tsup.config.base.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+// https://tsup.egoist.dev/
+export const baseConfig = defineConfig({
+  // keep test files from output
+  // keep all transpiled files to dist, this helps with deep imports
+  entry: [
+    'src/index.ts',
+  ],
+  // clean output directory before each build
+  clean: true,
+  dts: true,
+  // by default tsup will exclude monorepo dependencies
+  // but we wish to bundle them
+  // noExternal: [/@1ds/],
+  // noExternal: [ /(.*)/ ],
+});
