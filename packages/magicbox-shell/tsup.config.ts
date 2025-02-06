19bb9de0b3d5bbdf49d6870907968a12e57f2a4d
avanish.pathak <avanish.pathak@docusign.com>
2025-02-06 14:25:22 -0800

    feat(build): Add webpack handler for the Shell

---
+++ packages/magicbox-shell/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+import { baseConfig } from '../../tsup.config.base';
+
+export default defineConfig({
+  // Career hack: upgrade the target and get fired
+  ...baseConfig,
+  target: 'ESNext',
+  external: ['react', 'react-dom', 'lottie-web'],
+  format: ['esm', 'cjs'],
+});
