7686df80443336754f208217e83bbc2c5949a8e8
Luke Hatcher <lukehatcher98@gmail.com>
2025-05-09 12:51:14 -0700

    feat: switch alt test order

--- apps/starter-app/src/__tests__/tests/platform-utils/appLoadTime.spec.ts
+++ apps/starter-app/src/__tests__/tests/platform-utils/appLoadTime.spec.ts
@@ -1,45 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('Should successfully Mark Start and Mark End for generic-child-widget', async ({
-  page,
-}) => {
-  // page.on('console', (msg) => {
-  //   if (msg.type() === 'error') {
-  //     console.error(`Console error: ${msg.text()}`);
-  //   }
-  // });
-
-  page.on('request', (request) => {
-    const url = request.url();
-    if (url.includes('bundle.js')) {
-      console.log('🔍 Bundle URL:', url);
-    }
-  });
-
-  await page.goto('http://localhost:3001/app1/utils');
-
-  // const content = await page.content();
-  // console.log('Page HTML:', content);
-
-  await page.click('button[data-qa="utils.appLoadTime.getEntries.btn"]');
-
-  const resultElement = page.locator(
-    'div[data-qa="utils.appLoadTime.getEntries.result"]',
-  );
-
-  await expect(resultElement).not.toContainText('@1fe/starter-kit2-start');
-  await expect(resultElement).not.toContainText('@1fe/starter-kit2-end');
-
-  await page.click('button[data-qa="utils.appLoadTime.get.btn"]');
-
-  await page.waitForTimeout(500);
-
-  await page.click('button[data-qa="utils.appLoadTime.getEntries.btn"]');
-
-  await expect(resultElement).toContainText('@1fe/starter-kit2-start');
-  await expect(resultElement).toContainText('@1fe/starter-kit2-end');
-});
-
 test('Should get all entries', async ({ page }) => {
   page.on('request', (request) => {
     const url = request.url();
@@ -86,4 +46,50 @@ test('Should mark and measure custom events', async ({ page }) => {
   expect(
     /\d+(\.\d+)?/.test((await resultElement.innerText()) || ''),
   ).toBeTruthy();
+});
+
+test('Should successfully Mark Start and Mark End for generic-child-widget', async ({
+  page,
+  context,
+}) => {
+  // await context.route('**/bundle.js', async (route, request) => {
+  //   const response = await route.fetch(); // continue and get real response
+  //   const body = await response.text();
+  //   const firstLines = body.split('\n').slice(0, 5).join('\n');
+
+  //   console.log('📦 First 5 lines of bundle.js:\n' + firstLines);
+
+  //   // Continue with the original response
+  //   await route.fulfill({ response });
+  // });
+
+  page.on('request', (request) => {
+    const url = request.url();
+    if (url.includes('bundle.js')) {
+      console.log('🔍 Bundle URL:', url);
+    }
+  });
+
+  await page.goto('http://localhost:3001/app1/utils');
+
+  // const content = await page.content();
+  // console.log('Page HTML:', content);
+
+  await page.click('button[data-qa="utils.appLoadTime.getEntries.btn"]');
+
+  const resultElement = page.locator(
+    'div[data-qa="utils.appLoadTime.getEntries.result"]',
+  );
+
+  await expect(resultElement).not.toContainText('@1fe/starter-kit2-start');
+  await expect(resultElement).not.toContainText('@1fe/starter-kit2-end');
+
+  await page.click('button[data-qa="utils.appLoadTime.get.btn"]');
+
+  await page.waitForTimeout(500);
+
+  await page.click('button[data-qa="utils.appLoadTime.getEntries.btn"]');
+
+  await expect(resultElement).toContainText('@1fe/starter-kit2-start');
+  await expect(resultElement).toContainText('@1fe/starter-kit2-end');
 });
