6da60dbfd918ba70450c6ff8936cd6061fcb430f
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-06-09 12:32:45 -0700

    chore: better try catch

--- packages/create-1fe-app/src/commands/create/createAction.ts
+++ packages/create-1fe-app/src/commands/create/createAction.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 import { getLogger } from '../../lib/getLogger';
 import { getGitCloneUrl } from '../../lib/git-utils';
-import { RootCommandArgs, RootCommandOptions } from './root-command';
+import { RootCommandArgs, RootCommandOptions } from './createCommand';
 import { existsSync, rmSync } from 'fs';
 
 export async function rootAction(
@@ -19,38 +19,37 @@ export async function rootAction(
   try {
     logger.info('🔗 Cloning repository from:', cloneUrl);
     const response = await gitCloneAsync(cloneUrl, clonePath);
-
     logger.debug('Clone response:', response);
-
-    logger.info(`✅ Repository cloned successfully at ${clonePath}`);
-
-    logger.info('⚠️ Removing git history from the cloned repo');
-
-    const dotGitDir = join(clonePath, '.git');
-
-    if (existsSync(dotGitDir)) {
-      rmSync(join(clonePath, '.git'), {
-        recursive: true,
-      });
-      logger.info('✅ Git history removed successfully');
-    } else {
-      logger.warn('No .git directory found, skipping git history removal');
-    }
-
-    logger.log(
-      '🎉 Your 1fe app has been created successfully! 🎉',
-      `\n\nTo get started, navigate to the app directory:\n\n  cd ${appName}\n\n`,
-      'Then, install the dependencies:\n\n  yarn\n\n',
-      'Finally, start the development server:\n\n  yarn dev\n\n',
-      'Then navigate to http://localhost:3001 in your browser to see your new 1fe app in action!\n\n',
-      'Happy coding! 🚀\n',
-    );
   } catch (error) {
     logger.error('🛑 Failed to clone the repository:', error);
     throw new Error(
       'Failed to clone the repository. Please check the URL or your network connection.',
     );
   }
+
+  logger.info(`✅ Repository cloned successfully at ${clonePath}`);
+
+  logger.info('⚠️ Removing git history from the cloned repo');
+
+  const dotGitDir = join(clonePath, '.git');
+
+  if (existsSync(dotGitDir)) {
+    rmSync(join(clonePath, '.git'), {
+      recursive: true,
+    });
+    logger.info('✅ Git history removed successfully');
+  } else {
+    logger.warn('No .git directory found, skipping git history removal');
+  }
+
+  logger.log(
+    '🎉 Your 1fe app has been created successfully! 🎉',
+    `\n\nTo get started, navigate to the app directory:\n\n  cd ${appName}\n\n`,
+    'Then, install the dependencies:\n\n  yarn\n\n',
+    'Finally, start the development server:\n\n  yarn dev\n\n',
+    'Then navigate to http://localhost:3001 in your browser to see your new 1fe app in action!\n\n',
+    'Happy coding! 🚀\n',
+  );
 }
 
 function gitCloneAsync(cloneUrl: string, clonePath: string) {
